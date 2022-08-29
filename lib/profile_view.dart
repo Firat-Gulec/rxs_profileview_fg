e63c61f866864c1cf26ed57835019d2b6c8c4db9
Fırat-Gulec <firat_gulec@hotmail.com>
2022-08-29 17:13:46 +0300

    last work

--- lib/profile_view.dart
+++ lib/profile_view.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:rxs_profileview_fg/change_theme_button_widget.dart';
@@ -28,7 +29,7 @@ int value = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+      return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
       ),
@@ -76,58 +77,157 @@ int value = 0;
                   const SizedBox(
                     height: 10,
                   ),
-                  ...ListTile.divideTiles(
-                    color: Colors.white,
-                                      tiles: 
+                 
+                 
 
-                     [
-                      ProfileListItem(
-                        icon: Icons.ac_unit,
-                        text: 'Purchase History',
-                      ),
-                      ProfileListItem(
-                        icon: Icons.ac_unit,
-                        text: 'Help & Support',
-                      ),
-                      ProfileListItem(
-                        icon: Icons.ac_unit,
-                        text: 'Settings',
-                      ),
-                      ProfileListItem(
-                        icon: Icons.ac_unit,
-                        text: 'Invite a Friend',
-                      ),
-                      ProfileListItem(
-                        icon: Icons.ac_unit,
-                        text: 'Logout',
-                      ),
-                    ],
-                  ),
-ExpansionTile(
-                title: const Text('Transaction Details'),
-                textColor: Colors.white,
-                iconColor: Colors.white,
-                initiallyExpanded: true,
-                maintainState: true,
-                children: List.generate(paymentLabels.length, (index) {
-                  return ListTile(
-                      leading: Radio(
-                        //activeColor: kPrimaryColor,
-                        value: index,
-                        groupValue: 5,
-                        onChanged: (i) => setState(() => value = index),
-                      ),
-                      title: Text(
-                        paymentLabels[index],
-                      ));
-                }),
+   Row(
+              children: [
+                Icon(
+                  Icons.person,
+                  color: Colors.green,
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                Text(
+                  "Account",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            Divider(
+              height: 15,
+              thickness: 2,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            buildAccountOptionRow(context, "Change password"),
+            buildAccountOptionRow(context, "Content settings"),
+            buildAccountOptionRow(context, "Social"),
+            buildAccountOptionRow(context, "Language"),
+            buildAccountOptionRow(context, "Privacy and security"),
+            SizedBox(
+              height: 40,
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.volume_up_outlined,
+                  color: Colors.green,
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                Text(
+                  "Notifications",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            Divider(
+              height: 15,
+              thickness: 2,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            buildNotificationOptionRow("New for you", true),
+            buildNotificationOptionRow("Account activity", true),
+            buildNotificationOptionRow("Opportunity", false),
+            SizedBox(
+              height: 50,
+            ),/*
+            Center(
+              child: OutlineButton(
+                padding: EdgeInsets.symmetric(horizontal: 40),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                onPressed: () {},
+                child: Text("SIGN OUT",
+                    style: TextStyle(
+                        fontSize: 16, letterSpacing: 2.2, color: Colors.black)),
               ),
+*/
+                 
 
                   ElevatedButton(onPressed: () {}, child: Text('data')),
                   ChangeThemeButtonWidget()
                 ],
               ),
             )
+          ],
+        ),
+      ),
+    );
+  }
+
+
+
+Row buildNotificationOptionRow(String title, bool isActive) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          title,
+          style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+              color: Colors.grey[600]),
+        ),
+        Transform.scale(
+            scale: 0.7,
+            child: CupertinoSwitch(
+              value: isActive,
+              onChanged: (bool val) {},
+            ))
+      ],
+    );
+  }
+
+  GestureDetector buildAccountOptionRow(BuildContext context, String title) {
+    return GestureDetector(
+      onTap: () {
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text(title),
+                content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text("Option 1"),
+                    Text("Option 2"),
+                    Text("Option 3"),
+                  ],
+                ),
+                actions: [
+                  FlatButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text("Close")),
+                ],
+              );
+            });
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+                color: Colors.grey[600],
+              ),
+            ),
+            Icon(
+              Icons.arrow_forward_ios,
+              color: Colors.grey,
+            ),
           ],
         ),
       ),
