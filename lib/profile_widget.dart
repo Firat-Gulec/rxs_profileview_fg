dc73398b7f0f528abb9af2eaa8f5e5a6b8b62f78
Fırat-Gulec <firat_gulec@hotmail.com>
2022-09-01 16:51:33 +0300

    work

--- lib/profile_widget.dart
+++ lib/profile_widget.dart
@@ -1,48 +1,49 @@
 import 'package:flutter/material.dart';
 
-
 class ProfileWidget extends StatelessWidget {
-  final IconData icon;
-  final String title;
   const ProfileWidget({
     Key? key,
     required this.icon,
     required this.title,
+  //  required this.press,
   }) : super(key: key);
+ // final GestureTapCallback press;
+  final IconData icon;
+  final String title;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 18),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            children: [
-              Icon(
-                icon,
-               color: Colors.black.withOpacity(.2),
-                size: 24,
-              ),
-              const SizedBox(
-                width: 16,
-              ),
-              Text(
-                title,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w600,
+    return GestureDetector(
+      //onTap: press,
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 18),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Icon(
+                  icon,
+                  color: Colors.green, //.withOpacity(.2),
+                  size: 24,
                 ),
-              ),
-            ],
-          ),
-          Icon(
-            Icons.arrow_forward_ios,
-            color: Colors.black.withOpacity(.4),
-            size: 16,
-          )
-        ],
+                const SizedBox(
+                  width: 16,
+                ),
+                Text(title,
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.grey[600])),
+              ],
+            ),
+            Icon(
+              Icons.arrow_forward_ios,
+              color: Colors.grey[600],
+              size: 16,
+            )
+          ],
+        ),
       ),
     );
   }
